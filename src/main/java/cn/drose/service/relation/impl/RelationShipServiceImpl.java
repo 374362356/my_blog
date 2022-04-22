d4322afe9b76e311d0a921d87df81ed7398d7744
onemorenight <374362356@qq.com>
2022-04-22 17:11:26 +0800

    换包

---
+++ src/main/java/cn/drose/service/relation/impl/RelationShipServiceImpl.java
@@ -0,0 +1,18 @@
+package cn.drose.service.relation.impl;
+
+import cn.drose.dao.RelationShipDao;
+import cn.drose.service.relation.RelationShipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Donghua.Chen on 2018/4/30.
+ */
+@Service
+public class RelationShipServiceImpl implements RelationShipService {
+
+    @Autowired
+    private RelationShipDao relationShipDao;
+
+
+}
